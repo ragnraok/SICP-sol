e7b81d778f98edd184f2294752f5ba2de2e5e055
ragnraok <okone1288@gmail.com>
2012-07-17 14:24:30 +0800

    change 2.4

--- chapter-2/2.1/2.4.scm
+++ chapter-2/2.1/2.4.scm
@@ -10,4 +10,12 @@
 (define (cdr z)
  (z (lambda (p q) q)))
 
+;(car (cons 1 2))
+;(car (lambda (m) (m 1 2))
+;((lambda (z) (z (lambda (p q) p)))
+; 	(lambda (m) (m 1 2)))
+;((lambda (m) (m 1 2)) (lambda (p q) p))
+;((lambda (p q) p) (1 2))
+;1
+
 
